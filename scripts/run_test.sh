f14ee4f21f572ce6dc0b754567799dd2b63b5bb3
Surya Bhusal <surya@fetch.ly>
2023-05-31 11:01:13 +0545

    Added waits for container starting.

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-sleep 10 && selenium-side-runner Test.side --server http://selenium:4444/wd/hub
+# wait untill selenium driver starts
+while ! nc -z "selenium_driver" "4444"; do
+    echo "Selenium driver is not started yet, waiting..."
+    sleep 0.1
+done
+
+# wait untill frontend starts
+while ! nc -z "react_frontend" "3000"; do
+    echo "React frontend is not started yet, waiting..."
+    sleep 0.1
+done
+
+echo "both services started"
+
+selenium-side-runner Test.side --server http://selenium_driver:4444/wd/hub
+
+exec "@"
